7afde59269ba4eb442db100a9ca8a44940ac0664
SudhanshuCKY <30956347+SudhanshuCKY@users.noreply.github.com>
2022-02-10 17:16:21 +0530

    Add files via upload

---
+++ basic/7_basic_calculator.py
@@ -0,0 +1,14 @@
+# Make a calculator that takes input from user and do basic calculation using different operators
+
+val_1 = int(input("Enter first value : "))
+val_2 = int(input("Enter second value : "))
+
+print("Addition : ", val_1+val_2)
+
+print("Subtraction : ", val_1-val_2)
+
+print("Multiplication : ", val_1*val_2)
+
+print("Division : ", val_1/val_2)
+
+print("Remainder :", val_1%val_2)
