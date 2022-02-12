f96721677d9657050860a837d05fd9351cf8a0a5
SudhanshuCKY <30956347+SudhanshuCKY@users.noreply.github.com>
2022-02-12 22:07:42 +0530

    Add files via upload

---
+++ basic/8_swap_values.py
@@ -0,0 +1,11 @@
+# Program to swap two user inputs
+
+val_1 = input("Enter first value : ")
+val_2 = input("Enter second value : ")
+
+val_3=val_1
+val_1=val_2
+val_2=val_3
+
+print("Swapped value 1 is ",val_1)
+print("Swapped value 2 is ",val_2)
