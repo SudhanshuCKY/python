e199d5ff64eefaecc6b84379b896b3b261ee3942
SudhanshuCKY <30956347+SudhanshuCKY@users.noreply.github.com>
2022-02-13 11:50:16 +0530

    Add files via upload

---
+++ basic/9_triangle_area.py
@@ -0,0 +1,11 @@
+# Calculate the area of a right angled triangle
+
+print("Formula : 1/2 * base * height")
+
+height = float( input( "Enter the value of Height : "))
+base = float( input( "Enter the value of Base : "))
+
+area=0.5*base*height
+
+print("Area of right angled triangle : ", area)
+
